5ff3146728b17e53dfcffc327a8919fdedf6eb7e
vishnu <vishnushaji797@gmail.com>
2023-12-25 08:51:29 +0530

    25/12/2023

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
-import Navbar from './Components/Navbar';
+// import Navbar from './Components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './Components/Home';
-import Footer from './Components/Footer';
+import States from './Components/States';
+import { Container } from 'react-bootstrap'
+
+// import Home from './Components/Home';
+// import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
-       <Navbar />
+      {/* <h1>Hello world</h1> */}
+      <Container>       
+       <States name={"Kerala"}/>
+       <States name={"Tamil nadu"}/>
+       <States name={"Karnataka"}/>
+      </Container>
+
+
+       {/* <Navbar />
        <Home />
        <Home />
        <Home />
-       <Footer />
+       <Footer /> */}
     </div>
   );
 }
---
+++ src/Components/States.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const States = (props) => {
+  return (
+        <div>
+        <h1 className='bg-warning'>     
+        {props.name}
+        </h1> 
+        </div>
+  )
+}
+
+export default States
